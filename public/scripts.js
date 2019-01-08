aba9da3eaa33a6264da82c406cdc015372902f34
Thanas Pulaj <thanas2053@gmail.com>
2019-01-08 22:09:47 +0100

    CSS improvements for campground index

--- public/scripts.js
+++ public/scripts.js
@@ -31,16 +31,16 @@ $( "document" ).ready(function(){
 			const data = await response.json();
 			output.innerHTML = "";
 			data.campgrounds.forEach(function(campground){
-				output.innerHTML += `<div class="col-sm-12 col-md-3">
-					<div class="card my-3 text-center">
+				output.innerHTML += `
+				<div class="campground col-sm-12 col-md-4 col-lg-3">
+					<div class="card border-primary text-white my-3 text-center">
 						<img class="card-img-top" src="${campground.image}" class="img-fluid">
-						<hr class="mx-auto">
-						<div class="card-body ">
-							<h4 class="card-title">${campground.name}</h4>
-							<a class="card-text btn btn-sm btn-outline-info" href="/campgrounds/${campground._id}" class="btn btn-sm btn-primary">More Info</a>
+						<div class="card-body my-auto py-auto">
+							<a class="h4" href="/campgrounds/${campground._id}" class="btn btn-sm btn-primary">${campground.name}</a>
 						</div>
 					</div>
-				</div>`;
+				</div>
+				`;
 			});
 		} else {
 			output.innerHTML = origOutput;
