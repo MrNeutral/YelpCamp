134703e93c8b2674e8d1b63a8ceacda8e82673be
Thanas Pulaj <thanas2053@gmail.com>
2019-02-04 17:42:28 +0100

    AJAX campground edit

--- public/scripts.js
+++ public/scripts.js
@@ -1,30 +1,34 @@
 /*jshint ignore:start*/
-$( "document" ).ready(function(){
+$("document").ready(function() {
 	let origOutput;
-	if($(".alert.alert-success")){
+	if ($(".alert.alert-success")) {
 		setTimeout(() => $(".alert.alert-success").fadeOut(600, () => $(this).remove()), 2000);
 	}
-	if(document.getElementById("output")){
+	if (document.getElementById("output")) {
 		origOutput = document.getElementById("output").innerHTML;
 	}
 	$(".submit").on("click", function(e) {
-		if($(this).parent().prev().find("form").hasClass("edit-comment") && $(this).parent().prev().find("form").find("input").val() !== ""){
-			updateData.call($(this).parent().prev().find("input"), e, true);
+		if (($(this).parent().prev().find("form").hasClass("edit-comment"))
+			&& $(this).parent().prev().find("form").find("input").val() !== "") {
+			updateComment.call($(this).parent().prev().find("input"), e, true);
 		}
-		if($(this).parent().prev().find("form").hasClass("delete-campground")){
+		else if ($(this).parent().prev().find("form").hasClass("edit-campground")) {
+			updateCampground.call($(this).parent().prev().find("input"), e, true);
+		}
+		else if ($(this).parent().prev().find("form").hasClass("delete-campground")) {
 			$(this).parent().prev().find("form").submit();
 		}
-		if($(this).parent().prev().find("form").hasClass("delete-comment")){
-			deleteData.call($(this).parent().prev().find("form"));
+		else if ($(this).parent().prev().find("form").hasClass("delete-comment")) {
+			deleteComment.call($(this).parent().prev().find("form"));
 		}
 	});
 	$("#pills-tab a:first").tab("show");
 	$("#back").prop("href", document.referrer);
 	$("#searchForm").find("input").val("");
-	$("#searchForm").on("input", async function(){
+	$("#searchForm").on("input", async function() {
 		const input = $(this).find("input");
 		let output = document.getElementById("output");
-		if(input.val() !== ""){
+		if (input.val() !== "") {
 			const response = await fetch(`/campgrounds?search=${input.val()}`, {
 				headers: {
 					"Content-Type": "application/json",
@@ -33,7 +37,7 @@ $( "document" ).ready(function(){
 			});
 			const data = await response.json();
 			output.innerHTML = "";
-			data.campgrounds.forEach(function(campground){
+			data.campgrounds.forEach(function(campground) {
 				output.innerHTML += `
 				<div class="campground col-sm-12 col-md-4 col-lg-3">
 					<div class="card border-primary text-white my-3 text-center">
@@ -49,11 +53,14 @@ $( "document" ).ready(function(){
 			output.innerHTML = origOutput;
 		}
 	});
-	$(".edit-comment").find("input").on("keydown", function(e){
-		updateData.call($(this), e, false);
+	$(".edit-comment").find("input").on("keydown", function(e) {
+		updateComment.call($(this), e, false);
 	});
-	$(".input-group-prepend button").on("click", function(){
-		if($(this).parent().siblings().hasClass("custom-file")){
+	$(".edit-campground").find("input").on("keydown", function(e) {
+		updateCampground.call($(this).closest("form").find("input"), e, false);
+	});
+	$(".input-group-prepend button").on("click", function() {
+		if ($(this).parent().siblings().hasClass("custom-file")) {
 			$(this).closest("form").attr("enctype", "application/x-www-form-urlencoded");
 			$(this).parent().siblings().html(`
 					<input class="pl-3 form-control" type="url" name="campground[image]" placeholder="http://imgur.com/campground.jpg" required/>
@@ -68,7 +75,7 @@ $( "document" ).ready(function(){
 			`);
 			$(this).parent().siblings().removeClass("px-0");
 			$(this).parent().siblings().addClass("custom-file");
-			$(this).parent().siblings().find("input").on("change",function(){
+			$(this).parent().siblings().find("input").on("change", function() {
 				//get the file name
 				const fileName = $(this).val();
 				//replace the "Choose a file" label
@@ -76,7 +83,7 @@ $( "document" ).ready(function(){
 			});
 		}
 	});
-	$("#inputGroupFile03").on("change",function(){
+	$("#inputGroupFile03").on("change", function() {
 		//get the file name
 		// eslint-disable-next-line no-useless-escape
 		const fileName = $(this).val().replace(/^.*[\\\/]/, "");
@@ -84,14 +91,14 @@ $( "document" ).ready(function(){
 		$(this).next(".custom-file-label").html(fileName);
 	});
 });
-async function updateData(e, button){
-	if(e.which == 13 || button){
+async function updateComment(e, button) {
+	if (e.which == 13 || button) {
 		e.preventDefault();
 		const input = $(this);
 		input.closest(".modal.fade").modal("hide");
-		const output = input.closest(".modal.fade").siblings("p") ;
+		const output = input.closest(".modal.fade").siblings("p");
 		const commentData = input.closest(".edit-comment").serialize();
-		if(input.val() !== ""){
+		if (input.val() !== "") {
 			const response = await fetch(`${input.closest(".edit-comment").prop("action")}`, {
 				method: "PUT",
 				headers: {
@@ -106,7 +113,7 @@ async function updateData(e, button){
 	}
 }
 
-async function deleteData(){
+async function deleteComment() {
 	const form = $(this);
 	form.closest(".modal.fade").modal("hide");
 	$("body").removeClass("modal-open");
@@ -119,12 +126,43 @@ async function deleteData(){
 		}
 	});
 	const data = await response.json();
-	if(data.success){
-		if($(this).closest(".user").children().length == 1){
-			$(this).closest(".comment").remove();
-			$(".user").html("<span>There's nothing here...</span>");
-		} else {
-			$(this).closest(".comment").remove();
+	if (data.success) {
+		if ($(".user")) {
+			if ($(this).closest(".user").children().length == 1) {
+				$(this).closest(".comment").remove();
+				$(".user").html("<span>There's nothing here...</span>");
+			} else {
+				$(this).closest(".comment").remove();
+			}
+		}
+	}
+}
+
+async function updateCampground(e, button) {
+	if (e.which == 13 || button) {
+		e.preventDefault();
+		const inputs = $(this);
+		inputs.closest(".modal.fade").modal("hide");
+		let campgroundData = inputs.closest(".edit-campground").serialize();
+		for (let i = 0, counter = 0; i < inputs.length; i++) {
+			if (inputs[i].value != "" || i == inputs.length - 1) {
+				counter++;
+			}
+			if (counter == inputs.length && i == inputs.length - 1) {
+				const response = await fetch(`${inputs.closest(".edit-campground").prop("action")}`, {
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/x-www-form-urlencoded",
+						"X-Requested-With": "XMLHttpRequest"
+					},
+					body: campgroundData
+				});
+				const data = await response.json();
+				$(".campground-image").attr("src", data.campground.image);
+				$(".campground-price").text(data.campground.price);
+				$(".campground-description").text(data.campground.description);
+				$(".campground-name").text(data.campground.name);
+			}
 		}
 	}
 }
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,18 +1,18 @@
-const express 	= require("express"),
-	Campground  = require("../models/campground"),
-	router 		= express.Router(),
-	mdw 		= require("../middleware"),
+const express = require("express"),
+	Campground = require("../models/campground"),
+	router = express.Router(),
+	mdw = require("../middleware"),
 	// eslint-disable-next-line no-unused-vars
-	dotEnv		= require("dotenv").config(),
-	multer		= require("multer"),
-	cloudinary 	= require("cloudinary"),
-	{check, oneOf, validationResult} = require("express-validator/check"),
-	storage		= multer.diskStorage({
-		filename: function(req, file, callback){
-			callback(null, Date.now()+file.originalname);
+	dotEnv = require("dotenv").config(),
+	multer = require("multer"),
+	cloudinary = require("cloudinary"),
+	{ check, oneOf, validationResult } = require("express-validator/check"),
+	storage = multer.diskStorage({
+		filename: function(req, file, callback) {
+			callback(null, Date.now() + file.originalname);
 		}
 	}),
-	imageFilter				= function(req, file, cb){
+	imageFilter = function(req, file, cb) {
 		// accept image files only
 		if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
 			req.fileValidationError = "Forbidden extension";
@@ -20,50 +20,50 @@ const express 	= require("express"),
 		}
 		cb(null, true);
 	},
-	upload 		= multer({ storage: storage, fileFilter: imageFilter});
+	upload = multer({ storage: storage, fileFilter: imageFilter });
 
-	
-cloudinary.config({ 
-	cloud_name: "yelpcamp-thanaspulaj", 
-	api_key: process.env.CLOUDINARY_API_KEY, 
+
+cloudinary.config({
+	cloud_name: "yelpcamp-thanaspulaj",
+	api_key: process.env.CLOUDINARY_API_KEY,
 	api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 
-router.get("/", function (req, res) {
+router.get("/", function(req, res) {
 	if (req.query.search && req.query.search != "") {
 		const regex = new RegExp(escapeRegex(req.query.search), "gi");
 		Campground.find({ "name": regex }, function(err, found) {
-			if(err) {
+			if (err) {
 				console.log(err);
 			} else {
 				if (req.xhr) {
 					if (found.length < 1) {
-						res.json({campgrounds: null, flash: {type: "error",msg: "No campground found."}});
+						res.json({ campgrounds: null, flash: { type: "error", msg: "No campground found." } });
 					} else {
-						res.json({campgrounds: found});
+						res.json({ campgrounds: found });
 					}
 				} else {
-					res.render("campgrounds/index", {campgrounds: found});
+					res.render("campgrounds/index", { campgrounds: found });
 				}
 			}
-		}); 
+		});
 	} else {
-		Campground.find({}, function (err, found) {
+		Campground.find({}, function(err, found) {
 			if (err) {
 				console.log(err);
 				req.flash("error", "Something went wrong");
 				res.redirect("/");
 			} else {
-				res.render("campgrounds/index", {campgrounds: found});
+				res.render("campgrounds/index", { campgrounds: found });
 			}
 		});
 	}
 });
 
-router.post("/", mdw.isLoggedIn,[
+router.post("/", mdw.isLoggedIn, [
 	check("campground[name]").not().isEmpty(),
-	check("campground[name]").isLength({min:1, max: 34}),
+	check("campground[name]").isLength({ min: 1, max: 34 }),
 	oneOf([
 		check("campground[image]").not().isEmpty(),
 		check("campground[image]").isURL(),
@@ -73,16 +73,16 @@ router.post("/", mdw.isLoggedIn,[
 		check("campground[price]").not().isEmpty(),
 	], "Invalid value for price"),
 	check("campground[description]").not().isEmpty()
-], upload.single("campground[image]"), function(req, res){
+], upload.single("campground[image]"), function(req, res) {
 	let errors = validationResult(req);
-	if(req.fileValidationError){
+	if (req.fileValidationError) {
 		req.flash("error", "Invalid file type");
 		return res.redirect("/campgrounds/new");
 	}
 	if (!errors.isEmpty()) {
 		errors = errors.array();
 		errors = errors.map(error => {
-			if(error.param !== "_error"){
+			if (error.param !== "_error") {
 				return `${error.msg} for ${error.param.replace(/\./g, " ")}`;
 			} else {
 				return `${error.msg}`;
@@ -91,11 +91,11 @@ router.post("/", mdw.isLoggedIn,[
 		req.flash("error", errors.join("<br/>"));
 		return res.redirect("/campgrounds/new");
 	}
-	if(req.headers["content-type"].slice(0, 19) == "multipart/form-data"){
+	if (req.headers["content-type"].slice(0, 19) == "multipart/form-data") {
 		cloudinary.v2.uploader.upload(req.file.path, function(err, result) {
 			// add cloudinary url for the image to the campground object under image property
 			req.body.campground.image = result.secure_url;
-			Campground.create(req.body.campground, function (err, campground) {
+			Campground.create(req.body.campground, function(err, campground) {
 				if (err) {
 					console.log(err);
 					req.flash("error", err.message);
@@ -111,7 +111,7 @@ router.post("/", mdw.isLoggedIn,[
 			});
 		});
 	} else {
-		Campground.create(req.body.campground, function (err, campground) {
+		Campground.create(req.body.campground, function(err, campground) {
 			if (err) {
 				console.log(err);
 				req.flash("error", err.message);
@@ -128,14 +128,14 @@ router.post("/", mdw.isLoggedIn,[
 	}
 });
 
-router.get("/new", mdw.isLoggedIn, function (req, res) {
+router.get("/new", mdw.isLoggedIn, function(req, res) {
 	res.render("campgrounds/new");
 });
 
-router.get("/:id", function (req, res) {
+router.get("/:id", function(req, res) {
 	Campground.findOne({
 		"_id": req.params.id
-	}).populate("comments").exec(function (err, found) {
+	}).populate("comments").exec(function(err, found) {
 		if (err) {
 			console.log(err);
 			req.flash("error", "Campground not found");
@@ -164,25 +164,29 @@ router.get("/:id", function (req, res) {
 // 	});
 // });
 
-router.put("/:id", mdw.checkCampgroundOwnership, function (req, res) {
+router.put("/:id", mdw.checkCampgroundOwnership, function(req, res) {
 	Campground.findOneAndUpdate({
 		"_id": req.params.id
-	}, req.body.campground, function (err) {
+	}, req.body.campground, { new: true }, function(err, campground) {
 		if (err) {
 			console.log(err);
 			req.flash("error", err.message);
 			req.redirect("/campgrounds/" + req.params.id);
 		} else {
-			req.flash("success", "Campground updated");
-			res.redirect("/campgrounds/" + req.params.id);
+			if (req.xhr) {
+				res.json({ campground: campground });
+			} else {
+				req.flash("success", "Campground updated");
+				res.redirect("/campgrounds/" + req.params.id);
+			}
 		}
 	});
 });
 
-router.delete("/:id", mdw.checkCampgroundOwnership, function (req, res) {
+router.delete("/:id", mdw.checkCampgroundOwnership, function(req, res) {
 	Campground.findOneAndDelete({
 		"_id": req.params.id
-	}, function (err) {
+	}, function(err) {
 		if (err) {
 			console.log(err);
 			req.flash("error", err.message);
